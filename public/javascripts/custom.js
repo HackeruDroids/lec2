fadbbb872e21e4e4e33d27d14cf17692824158e8
hackeru <hackeru@hackeru4.local>
2016-12-19 20:02:14 +0200

    js objects

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,1 +1,32 @@
 //alert('Hello');
+
+var x = 10.1; //Number
+var s = "This is a string"; //String
+var b = true; //boolean
+
+
+var p1 = {
+    firstName: "Moshe",
+    phone: "050-7123012",
+    hasegPratim: function () {
+        alert(this.firstName);
+    },
+    sayHello: function(bracha){
+        alert(bracha + " " + this.firstName);
+    }
+};
+
+//p1.hasegPratim();
+p1.sayHello('Shalom');
+
+
+// p1.firstName = "Tomer";
+// //alert("Name: " + p1.firstName + " Phone: " + p1.phone);
+
+
+// alert(p1["firstName"]);
+
+// //this is a function
+// function myfun() {
+
+// }
