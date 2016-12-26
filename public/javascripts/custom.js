2b9d7927df3a81a3a5e2cc5def2bf8cd969afe3e
hackeru <hackeru@hackeru4.local>
2016-12-26 21:23:17 +0200

    routing

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,153 +1,76 @@
-//alert('Hello');
+//find an element by id:
+var myBtn = document.getElementById('myBtn');
 
-var x = 10.1; //Number
-var s = "This is a string"; //String
-var b = true; //boolean
+function btnClicked(){
+  var num1 = document.getElementById('num1');
+  var num2 = document.getElementById('num2');
+  
+  var x = Number(num1.value);
+  var y = Number(num2.value);
+
+  alert(x + y);
+  
+}
+
+                      //event name, function
+myBtn.addEventListener('click', btnClicked);
 
 
-// var p1 = {
-//     firstName: "Moshe",
-//     phone: "050-7123012",
-//     hasegPratim: function () {
-//         alert(this.firstName);
-//     },
-//     sayHello: function(bracha){
-//         alert(bracha + " " + this.firstName);
+// myBtn.addEventListener('click', function(){
+//   alert('You clicked me');
+// });
+
+// function getRandomColor(){
+//     var letters = '0123456789abcdef';
+//     var arr = letters.split('');
+
+
+//     var color = '#';
+//     for(var i = 0; i < 6; i++){
+//       var rand = Math.random()*16;
+//       var randIndex  = Math.floor(rand);
+//       color = color + arr[randIndex];
 //     }
-// };
 
-
-
-// var p2 = {
-//     firstName: "Tomer",
-//     phone: "050-7123012",
-//     hasegPratim: function () {
-//         alert(this.firstName);
-//     },
-//     sayHello: function(bracha){
-//         alert(bracha + " " + this.firstName);
-//     }
-// };
-
-// //p1.hasegPratim();
-// p1.sayHello('Shalom');
-
-
-
-
-
-
-
-
-
-var div = document.getElementById("mydiv");
-
-
-
-
-
-
-
-
-
-
-
-
-
-function add(x, y){
-    //return x + y;
-    alert(x + y);
-}
-
-
-function Person(firstName, phone, age){
-    //save the data 
-    this.firstName = firstName;
-    this.phone = phone;
-    this.age = age;
-    //display the details of a specific person
-    this.dispDetails = function(){
-        alert(this.firstName + " " + this.phone + " " + this.age);
-    }
-
-    this.addYears = function(x){
-        //this.age = this.age + x;
-        this.age += x;
-    }
-}
-
-
-var p1 = new Person("Tomer", "050-7123012", 18);
-var p2 = new Person("Moshe", "050-7123012", 16);
-
-
-var details = "";
-for (var propertyName in p1) {
-        var element = p1[propertyName];
-        details+= element;
-}
-alert(details);
-
-var q1 = "What's the prior name of Shimon Peres?";
-var answers = ["Parnas", "Perszon", "Pertz", "Presky"];
-var correctAnsIndex = 3;
-
-function TriviaQuestion(question, answers, correctAnsIndex){
-    this.question = question;
-    this.answers = answers;
-    this.correctAnsIndex = correctAnsIndex;
-}
-
-
-var question = new TriviaQuestion("1+1=?", ["1", "2", "3", "4"], 1);
-
-
-
-
-
-//print all the even numbers between 0-100 ()
-var arr = [];
-for(var i = 0; i<= 100; i+=2){
-    //add an item to the array:
-    arr.push(i);
-}
-
-//print
-for(var i = 0; i< arr.length; i++){
-    console.log(arr[i]);
-}
-
-        //[0][1][2][3][4]
-var arr = [1, 2, 3, 4, 5];
-alert(arr[4]);
-
-var arr2 = new Array();
-arr2.push("1");
-arr2.push(2);
-console.log('Hello, Console');
-
-//alert(arr[99]);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// p1.firstName = "Tomer";
-// //alert("Name: " + p1.firstName + " Phone: " + p1.phone);
-
-
-// alert(p1["firstName"]);
-
-// //this is a function
-// function myfun() {
-
+//     return color;
 // }
+
+// var c = getRandomColor();
+// //find an element by it's ID
+// var myParagraph = document.getElementById('myParagraph');
+
+// myParagraph.style.color = c;
+
+
+
+// function add(){
+//   var sum = 0;
+//   for(var i = 0; i < arguments.length; i++){
+//       sum += arguments[i];
+//   }
+//   return sum;
+// }
+
+// var result = add(3," ", 5);
+
+// myParagraph.innerHTML += result;
+ 
+
+//  function sayHello(param1){
+
+//  }
+
+/*
+//find an element by id
+<p id="myParagraph">Hello</p>
+var myParagraph = document.getElementById('myParagraph');
+
+//change some html:
+myParagraph.innerHTML += result;
+
+
+//change the style:
+myParagraph.style.color = '#0f0f0f';
+ */
+
+
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,42 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { firstName:"Tomer", phone:"0507123012" });
-});
 
 
-//Tomer
-router.get('/tomer', function(req, res, next) {
-  res.render('tomer', { title: 'Express' });
-});
+var cars = [
+  {
+   vendor: "Renault",
+   model:'Megan',
+   color: 'red'
+  },
+  {
+   vendor: "Fiat",
+   model:'Punto',
+   color: 'Blue'
+  },
+  {
+   vendor: "Ford",
+   model:'Mustang',
+   color: 'red'
+  }
+];
+  
+  //get from database//
+  var obj = {user:'tomer', arr:cars, vendors:[1, 2, 3]};
+
+
+ router.get('/',indexPage);
+
+ 
+ function indexPage(req, res) {
+    res.render('index',obj);
+ }
+
+ router.get('/tomer',tomerPage);
+
+ function tomerPage(req, res) {
+    res.render('tomer', {title: "Express" });
+ }
+ 
 
 module.exports = router;
